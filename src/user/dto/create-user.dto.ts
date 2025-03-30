c5ca4c5dada483ba5e3b028651e8f6477871ca4a
Abdelrhman <abdo612200320@gmail.com>
2025-03-30 11:24:20 +0200

    update create-user.dto.ts

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
 import * as validate from 'class-validator';
 
 export class CreateUserDto {
