fd230dd832a026d72aa2ebbd473d294c6c6407ab
Abdelrhman <abdo612200320@gmail.com>
2025-04-08 15:09:36 +0200

    update sub-catergory.module.ts

--- src/sub-catergory/sub-catergory.module.ts
+++ src/sub-catergory/sub-catergory.module.ts
@@ -3,11 +3,13 @@ import { SubCatergoryService } from './sub-catergory.service';
 import { SubCatergoryController } from './sub-catergory.controller';
 import { subCategory, subCategorySchema } from './schemas/category.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from 'src/category/schemas/category.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: subCategory.name, schema: subCategorySchema },
+      { name: Category.name, schema: CategorySchema },
     ]),
   ],
   controllers: [SubCatergoryController],
