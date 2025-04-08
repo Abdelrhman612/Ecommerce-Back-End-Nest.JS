1ade23aee08d3d391428498eea47255865d7e1f4
Abdelrhman <abdo612200320@gmail.com>
2025-04-08 14:59:34 +0200

    feat: create category.schema.ts

---
+++ src/sub-catergory/schemas/category.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import { Category } from 'src/category/schemas/category.schema';
+
+export type UserDocument = subCategory & Document;
+
+@Schema({ timestamps: true })
+export class subCategory {
+  @Prop({
+    required: [true, 'The name field is required.'],
+    type: String,
+    trim: true,
+  })
+  name: string;
+  @Prop({ type: mongoose.Schema.ObjectId, ref: Category.name, required: true })
+  category: string;
+}
+
+export const subCategorySchema = SchemaFactory.createForClass(subCategory);
