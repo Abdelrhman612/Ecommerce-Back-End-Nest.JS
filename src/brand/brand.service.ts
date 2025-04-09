9d2f6a71a9b61808c5223ca91d78fb775ac9c7af
Abdelrhman <abdo612200320@gmail.com>
2025-04-09 11:45:03 +0200

    update brand.service.ts

--- src/brand/brand.service.ts
+++ src/brand/brand.service.ts
@@ -2,7 +2,7 @@ import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Brand } from './schemas/category.schema';
+import { Brand } from './schemas/brand.schema';
 import { Model } from 'mongoose';
 
 @Injectable()
