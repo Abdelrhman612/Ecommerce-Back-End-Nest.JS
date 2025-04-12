46b7bb7f250bff59f68581e86d80ede766c4545c
Abdelrhman <abdo612200320@gmail.com>
2025-04-12 23:14:06 +0200

    update category.schema.ts

--- src/category/schemas/category.schema.ts
+++ src/category/schemas/category.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export type UserDocument = Category & Document;
+export type CategoryDocument = Category & Document;
 
 @Schema({ timestamps: true })
 export class Category {
