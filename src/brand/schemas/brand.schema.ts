4a1e4f81d58e002e5468528954a05f4c2cbd2f07
Abdelrhman <abdo612200320@gmail.com>
2025-04-12 23:14:01 +0200

    update brand.schema.ts

--- src/brand/schemas/brand.schema.ts
+++ src/brand/schemas/brand.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export type UserDocument = Brand & Document;
+export type BrandDocument = Brand & Document;
 
 @Schema({ timestamps: true })
 export class Brand {
