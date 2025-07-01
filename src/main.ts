32dcc7a3d1d62824ca4aefdb4a01f19ac3214eb9
Abdelrhman <abdo612200320@gmail.com>
2025-07-01 03:43:23 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, hostname ? hostname() : '0.0.0.0');
 }
 void bootstrap();
